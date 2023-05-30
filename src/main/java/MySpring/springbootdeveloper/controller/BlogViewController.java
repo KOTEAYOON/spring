8ab966b16fb0a8f78dfd549402739ce00cfdc0d5
Tae Yoon <go2205@naver.com>
2023-05-30 22:44:45 +0900

    5/30

---
+++ src/main/java/MySpring/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package MySpring.springbootdeveloper.controller;
+
+import MySpring.springbootdeveloper.dto.ArticleListViewResponse;
+import MySpring.springbootdeveloper.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticle(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+        return "articleList";
+    }
+}
--- src/main/java/MySpring/springbootdeveloper/domain/Article.java
+++ src/main/java/MySpring/springbootdeveloper/domain/Article.java
@@ -4,6 +4,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -32,6 +36,13 @@ public class Article {
         this.content = content;
     }
 
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
 
 
